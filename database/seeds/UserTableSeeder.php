e6d42e38a242d2fced9fd18984f2ae8df63191f6
丁家财 <dingjiacai@dingjiacaideMacBook-Pro.local>
2015-12-25 14:12:39 +0800

    add user's data

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create(array('name'=>'admin','email'=>'dingjc89@126.com','password'=>Hash::make('admin')));
+    }
+}
